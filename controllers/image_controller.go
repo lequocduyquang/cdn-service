21051ac79e672f6c03188c6c1092cccd6ea57a80
lequocduyquang <quang.le@vexere.com>
2020-11-26 00:39:13 +0700

    add api upload image

--- controllers/image_controller.go
+++ controllers/image_controller.go
@@ -1,12 +1,15 @@
 package controllers
 
 import (
-	"io"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lequocduyquang/cdn-service/db"
+	"github.com/lequocduyquang/cdn-service/utils"
+	"go.mongodb.org/mongo-driver/mongo/gridfs"
 )
 
 var (
@@ -24,24 +27,30 @@ type imageController struct{}
 func (i *imageController) Upload(c *gin.Context) {
 	file, header, err := c.Request.FormFile("file")
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		restErr := utils.NewBadRequestError(err.Error())
+		c.JSON(restErr.Status(), restErr)
 		return
 	}
 	defer file.Close()
 
-	out, err := os.Create("public/" + header.Filename)
+	bucket, err := gridfs.NewBucket(db.Client.Database("jungleDB"))
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	defer out.Close()
-	_, err = io.Copy(out, file)
+	uploadStream, err := bucket.OpenUploadStream(header.Filename)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
+	defer uploadStream.Close()
 
-	// data, err := ioutil.ReadAll(file)
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileSize, err := uploadStream.Write(data)
+
 	c.JSON(http.StatusOK, gin.H{
 		"file_name": header.Filename,
+		"file_size": fileSize,
 	})
 }
