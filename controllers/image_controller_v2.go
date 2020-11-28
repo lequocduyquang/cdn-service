caa1a3f1a6a2f946a04bd8045e9f6663ede4b7c4
lequocduyquang <quang.le@vexere.com>
2020-11-28 16:29:27 +0700

    add api upload image v2

--- controllers/image_controller_v2.go
+++ controllers/image_controller_v2.go
@@ -1,11 +1,15 @@
 package controllers
 
 import (
+	"io"
 	"net/http"
+	"net/url"
 
+	"cloud.google.com/go/storage"
 	"github.com/gin-gonic/gin"
-	"github.com/lequocduyquang/cdn-service/services"
 	"github.com/lequocduyquang/cdn-service/utils"
+	"google.golang.org/api/option"
+	"google.golang.org/appengine"
 )
 
 var (
@@ -20,7 +24,24 @@ type ImageControllerV2Interface interface {
 
 type imageControllerV2 struct{}
 
+var (
+	storageClient *storage.Client
+)
+
 func (i *imageControllerV2) Upload(c *gin.Context) {
+	var err error
+	bucket := "cdn_lotus_bucket"
+	ctx := appengine.NewContext(c.Request)
+
+	storageClient, err = storage.NewClient(ctx, option.WithCredentialsFile("lotus-fitness.json"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"error":   true,
+		})
+		return
+	}
+
 	file, header, err := c.Request.FormFile("file")
 	if err != nil {
 		restErr := utils.NewBadRequestError(err.Error())
@@ -29,14 +50,56 @@ func (i *imageControllerV2) Upload(c *gin.Context) {
 	}
 	defer file.Close()
 
-	fileName, fileSize, err := services.ImageService.Upload(file, *header)
-	if err != nil {
-		restErr := utils.NewBadRequestError(err.Error())
-		c.JSON(restErr.Status(), restErr)
+	sw := storageClient.Bucket(bucket).Object(header.Filename).NewWriter(ctx)
+
+	if _, err := io.Copy(sw, file); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"error":   true,
+		})
 		return
 	}
+
+	if err := sw.Close(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"error":   true,
+		})
+		return
+	}
+
+	resultURL, err := url.Parse("/" + bucket + "/" + sw.Attrs().Name)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+			"Error":   true,
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"file_name": fileName,
-		"file_size": fileSize,
+		"message":  "file uploaded successfully",
+		"pathname": resultURL.EscapedPath(),
 	})
+
+	/*
+		file, header, err := c.Request.FormFile("file")
+		if err != nil {
+			restErr := utils.NewBadRequestError(err.Error())
+			c.JSON(restErr.Status(), restErr)
+			return
+		}
+		defer file.Close()
+
+		fileName, fileSize, err := services.ImageService.Upload(file, *header)
+		if err != nil {
+			restErr := utils.NewBadRequestError(err.Error())
+			c.JSON(restErr.Status(), restErr)
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"file_name": fileName,
+			"file_size": fileSize,
+		})
+	*/
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/lequocduyquang/cdn-service
 go 1.15
 
 require (
+	cloud.google.com/go/storage v1.12.0
 	github.com/gin-gonic/gin v1.6.3
 	github.com/google/uuid v1.1.2
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/mojocn/base64Captcha v1.3.1 // indirect
 	go.mongodb.org/mongo-driver v1.4.3
 	golang.org/x/image v0.0.0-20200927104501-e162460cd6b5 // indirect
+	google.golang.org/api v0.35.0
+	google.golang.org/appengine v1.6.7
 )
